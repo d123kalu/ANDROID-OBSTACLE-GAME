b9c9e08bb160aefdadf191b76119adfd87d59838
Dikachi <dikachik@yahoo.com>
2017-12-05 16:55:21 -0500

    Currently function but havent implemented the speed up part yet

---
+++ Tutorial/app/src/main/java/com/example/kalu/tutorial/GameObject.java
@@ -0,0 +1,12 @@
+package com.example.kalu.tutorial;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Kalu on 10/31/2017.
+ */
+
+public interface GameObject {
+    public void draw(Canvas canvas);
+    public void update();
+}
---
+++ Tutorial/app/src/main/java/com/example/kalu/tutorial/RectPlayer.java
@@ -0,0 +1,45 @@
+package com.example.kalu.tutorial;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+/**
+ * Created by Kalu on 10/31/2017.
+ */
+
+public class RectPlayer implements GameObject {
+    private Rect rectangle;
+    private int color;
+
+    public Rect getRectangle()
+    {
+        return rectangle;
+    }
+
+    public RectPlayer(Rect rectangle, int color)
+    {
+        this.rectangle = rectangle;
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rectangle, paint);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public void update(Point point)
+    {
+        rectangle.set(point.x - rectangle.width()/2, point.y - rectangle.height()/2, point.x + rectangle.width()/2, point.y + rectangle.height()/2);
+    }
+}
+
